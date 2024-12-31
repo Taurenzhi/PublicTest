100a3f4c2ac60ba7eb578966210ad2be8a86bf50
Alex Liu <alex.liu@cloudkitchens.com>
2024-12-31 16:11:55 +0800

    Update changes

---
+++ PublicTest/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+
+Console.WriteLine();
+Console.WriteLine("GetEnvironmentVariables: ");
+foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
+    Console.WriteLine("  {0} = {1}", de.Key, de.Value);
